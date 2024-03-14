bf91d88e6af7e77bdd2063ef30e6e03ae306ec87
kllygh <c.kellygoh@gmail.com>
2024-03-14 19:39:22 +0800

    Update CustomUserDetailsService.java

--- ticket-booking-system/src/main/java/com/is442g2t1/ticketbookingsystem/security/CustomUserDetailsService.java
+++ ticket-booking-system/src/main/java/com/is442g2t1/ticketbookingsystem/security/CustomUserDetailsService.java
@@ -6,7 +6,6 @@ import com.is442g2t1.ticketbookingsystem.User.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,26 +13,38 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
-public class CustomUserDetailsService implements UserDetailsService{
+public class CustomUserDetailsService implements UserDetailsService {
     private UserRepository userRepository;
 
     @Autowired
-    public CustomUserDetailsService(UserRepository userRepository){
+    public CustomUserDetailsService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         UserEntity user = userRepository.findByEmail(email)
-        .orElseThrow(() -> new UsernameNotFoundException("Email was not found"));
-        return new User(user.getEmail(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+                .orElseThrow(() -> new UsernameNotFoundException("Email was not found"));
+
+        return buildUserDetails(user);
     }
-    
-    // converts each Role into a SimpleGrantedAuthority object (a type provided by Spring Security), and collects these authorities into a list
-    private Collection<GrantedAuthority> mapRolesToAuthorities(List<Role> roles) {
-        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+
+    private UserDetails buildUserDetails(UserEntity user) {
+        // Extracting user roles and converting them to Spring Security authorities
+        Collection<GrantedAuthority> authorities = mapRolesToAuthorities(user.getRole());
+
+        // Creating UserDetails object using user details and authorities
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
+    }
+
+    // Convert Role to GrantedAuthority
+    private Collection<GrantedAuthority> mapRolesToAuthorities(Role role) {
+        return List.of(new SimpleGrantedAuthority(role.getName())); //https://docs.spring.io/spring-security/site/docs/current/api/org/springframework/security/core/authority/SimpleGrantedAuthority.html
     }
 }
