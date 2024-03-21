2656a510dd8b4ade06095fb3a8779957d02be97f
JXChin02 <jxchin.2021@scis.smu.edu.sg>
2024-03-21 14:34:26 +0800

    Create JWTAuthEntryPoint.java

---
+++ ticket-booking-system/src/main/java/com/is442g2t1/ticketbookingsystem/security/jwt/JWTAuthEntryPoint.java
@@ -0,0 +1,35 @@
+package com.is442g2t1.ticketbookingsystem.security.jwt;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component // allow it to be automatically detected and instantiated during the application's startup
+public class JWTAuthEntryPoint implements AuthenticationEntryPoint {
+
+    @Override // called when an authentication failure occurs. 
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+
+        final Map<String, Object> body = new HashMap<>();
+        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+        body.put("error", "Unauthorized");
+        body.put("message", authException.getMessage());
+        body.put("path", request.getServletPath());
+
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), body);
+    }
+}
