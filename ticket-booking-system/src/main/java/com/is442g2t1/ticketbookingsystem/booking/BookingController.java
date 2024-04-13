c691c20a906e2d15e088348fb1b94bd888d31794
eoyj <euniceong.2021@scis.smu.edu.sg>
2024-04-13 10:22:47 +0800

    fix

--- ticket-booking-system/src/main/java/com/is442g2t1/ticketbookingsystem/booking/BookingController.java
+++ ticket-booking-system/src/main/java/com/is442g2t1/ticketbookingsystem/booking/BookingController.java
@@ -3,7 +3,6 @@ package com.is442g2t1.ticketbookingsystem.booking;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = { "http://127.0.0.1:5173","http://localhost:5173"})
 @RestController
 @CrossOrigin(origins = {"http://127.0.0.1:5173", "http://localhost:5173"})
 @RequestMapping("/booking")
