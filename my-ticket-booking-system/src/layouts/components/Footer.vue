be1b5d2f03d510499d078d6e88b227c84980c0f4
JXChin02 <jxchin.2021@scis.smu.edu.sg>
2024-04-14 01:10:17 +0800

    ui design edits :D

--- my-ticket-booking-system/src/layouts/components/Footer.vue
+++ my-ticket-booking-system/src/layouts/components/Footer.vue
@@ -2,6 +2,10 @@
   <div class="h-100 d-flex flex-wrap align-center justify-space-between">
     <!-- 👉 Footer: left content -->
     <span class="d-flex align-center">
+    </span>
+
+    <!-- 👉 Footer: right content -->
+    <span class="d-md-flex gap-x-4 text-primary d-none">
       &copy;
       {{ new Date().getFullYear() }}
       Made With
@@ -11,35 +15,7 @@
         size="1.25rem"
         class="mx-1"
       />
-      By <a
-        href="https://themeselection.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="text-primary ms-1"
-      >ThemeSelection</a>
-    </span>
-    <span class="d-flex align-center">
-      Distributed By <a
-        href="https://themewagon.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="text-primary ms-1"
-      >ThemeWagon</a>
-    </span>
-    <!-- 👉 Footer: right content -->
-    <span class="d-md-flex gap-x-4 text-primary d-none">
-      <a
-        href="https://themeselection.com/license/"
-        target="noopener noreferrer"
-      >License</a>
-      <a
-        href="https://themeselection.com/"
-        target="noopener noreferrer"
-      >More Themes</a>
-      <a
-        href="https://demos.themeselection.com/sneat-vuetify-vuejs-admin-template/documentation/"
-        target="noopener noreferrer"
-      >Documentation</a>
+      By IS442 G2T1
     </span>
   </div>
 </template>
