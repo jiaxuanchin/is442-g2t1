20692ecc2eb7e51854dfba89beefdb7d7b1a157f
JXChin02 <jxchin.2021@scis.smu.edu.sg>
2024-04-14 12:07:01 +0800

    update sql

--- ticket-booking-system/ticket-booking-system.sql
+++ ticket-booking-system/ticket-booking-system.sql
@@ -12,7 +12,7 @@ SET time_zone = "+08:00";
 SET FOREIGN_KEY_CHECKS= 0;
 
 --
--- Database: `heartchat_db`
+-- Database: `ticket-booking-system`
 --
 
 CREATE DATABASE IF NOT EXISTS `ticket-booking-system` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
@@ -154,38 +154,35 @@ INSERT INTO Users (user_id, role_id, user_fname,user_lname,email, `password`, us
 
 INSERT INTO Event (event_id, event_title, event_date, event_description, event_location, event_start_time, event_end_time, filled, capacity, ticket_price, cancel_fee) VALUES
 -- 5 events within 24 hours
-(1, 'Musical', '2024-04-15', "Cats the Musical is coming to town! Cats is a sung-through musical with music by Andrew Lloyd Webber. It is based on the 1939 poetry collection Old Possum's Book of Practical Cats by T. S. Eliot.", 'The Capitol', '18:00', '21:00', 0, 2000, 150.00, 20.00),
-(2, 'Movie', '2024-04-15', 'Avengers 100', 'Golden Village (Plaza Singapura)', '14:45', '18:00', 0, 2000, 12.00, 20.00),
-(3, 'Seminar', '2024-04-15', 'How to make use of ChatGPT', 'Singapore Management University', '09:00', '13:00', 0, 40, 500.00, 20.00),
-(4, 'Concert', '2024-04-15', 'Disney is coming to town! Meet Mickey, Minney, Else, Anna and more', 'National Stadium', '19:30', '23:00', 498, 500, 150.00, 20.00),
-(5, 'Play', '2024-04-15', 'Romeo & Juliet is a tragedy written by William Shakespeare early in his career about the romance between two Italian youths from feuding families.', 'Esplanade', '19:00', '22:00', 276, 300, 78.00, 20.00),
+(1, 'Musical', '2024-04-15', "Cats the Musical is coming to town! Cats is a sung-through musical with music by Andrew Lloyd Webber. It is based on the 1939 poetry collection Old Possum\'s Book of Practical Cats by T. S. Eliot.", 'The Capitol', '18:00', '21:00', 93, 100, '150.00', '20.00'),
+(2, 'Movie', '2024-04-14', 'Avengers 100', 'Golden Village (Plaza Singapura)', '14:45', '18:00', 10, 2000, '12.00', '20.00'),
+(3, 'Seminar', '2024-04-16', 'How to make use of ChatGPT', 'Singapore Management University', '09:00', '13:00', 13, 40, '500.00', '20.00'),
+(4, 'Concert', '2024-04-15', 'Disney is coming to town! Meet Mickey, Minney, Else, Anna, Cinderella and more', 'National Stadium', '19:30', '23:00', 10, 200, '150.00', '20.00'),
+(5, 'Play', '2024-04-15', 'Romeo & Juliet is a tragedy written by William Shakespeare early in his career about the romance between two Italian youths from feuding families.', 'Esplanade', '19:00', '22:00', 499, 500, '78.00', '20.00'),
 -- 5 events after 6 months
-(6, 'Music Festival', '2025-04-14', 'Annual music festival featuring top artists', 'Marina Bay Sands', '09:00', '23:00', 0, 5000, 200.00, 30.00),
-(7, 'Tech Conference', '2026-10-05', 'Explore the latest trends in technology', 'Suntec Convention Centre', '09:00', '17:00', 0, 1000, 300.00, 50.00),
-(8, 'Food Expo', '2026-05-12', 'Sample cuisines from around the world', 'Singapore Expo', '11:00', '20:00', 0, 8000, 50.00, 10.00),
-(9, 'Fashion Show', '2026-08-28', 'Showcasing the latest fashion trends', 'Resorts World Sentosa', '19:00', '22:00', 0, 2000, 100.00, 20.00),
-(10, 'Sports Tournament', '2026-09-10', 'National sports tournament', 'Singapore Sports Hub', '10:00', '18:00', 0, 3000, 75.00, 15.00),
+(6, 'Music Festival', '2025-04-14', 'Annual music festival featuring top artists', 'Marina Bay Sands', '09:00', '23:00', 5, 5000, '200.00', '30.00'),
+(7, 'Tech Conference', '2026-10-05', 'Explore the latest trends in technology', 'Suntec Convention Centre', '09:00', '17:00', 3, 1000, '300.00', '50.00'),
+(8, 'Food Expo', '2026-05-12', 'Sample cuisines from around the world', 'Singapore Expo', '11:00', '20:00', 7, 150, '50.00', '10.00'),
+(9, 'Fashion Show', '2026-08-28', 'Showcasing the latest fashion trends', 'Resorts World Sentosa', '19:00', '22:00', 3, 2000, '100.00', '20.00'),
+(10, 'Sports Tournament', '2026-09-10', 'National sports tournament', 'Singapore Sports Hub', '10:00', '18:00', 9, 3000, '75.00', '15.00'),
 -- 5 events within in the past
-(11, 'Art Exhibition', '2023-03-15', 'Explore contemporary art pieces', 'ArtScience Museum', '10:00', '19:00', 0, 1500, 20.00, 5.00),
-(12, 'Comedy Show', '2023-06-02', 'An evening of laughter with top comedians', 'Capitol Theatre', '20:00', '22:30', 0, 1000, 40.00, 10.00),
-(13, 'Fitness Expo', '2023-02-08', 'Discover the latest in fitness and wellness', 'Singapore Expo', '09:00', '18:00', 0, 5000, 30.00, 5.00),
-(14, 'Technology Summit', '2023-09-20', 'Bringing together tech leaders and innovators', 'Marina Bay Sands', '08:30', '17:30', 0, 2000, 250.00, 50.00),
-(15, 'Culinary Workshop', '2023-11-12', 'Learn to cook gourmet dishes from renowned chefs like Gordon Ramsay and more!', 'Culinary Academy', '14:00', '17:00', 0, 50, 150.00, 30.00);
+(11, 'Art Exhibition', '2023-03-15', 'Explore contemporary art pieces', 'ArtScience Museum', '10:00', '19:00', 5, 1500, '20.00', '5.00'),
+(12, 'Comedy Show', '2023-06-02', 'An evening of laughter with top comedians', 'Capitol Theatre', '20:00', '22:30', 6, 1000, '40.00', '10.00'),
+(13, 'Fitness Expo', '2023-02-08', 'Discover the latest in fitness and wellness', 'Singapore Expo', '09:00', '18:00', 7, 5000, '30.00', '5.00'),
+(14, 'Technology Summit', '2023-09-20', 'Bringing together tech leaders and innovators', 'Marina Bay Sands', '08:30', '17:30', 7, 2000, '250.00', '50.00'),
+(15, 'Culinary Workshop', '2023-11-12', 'Learn to cook gourmet dishes from renowned chefs like Gordon Ramsay and more!', 'Culinary Academy', '14:00', '17:00', 4, 50, '150.00', '30.00');
 
 
 INSERT INTO Booking (user_id, event_id, number_of_tickets, booking_timestamp) VALUES
 -- Bookings within 24 hours
-(1, 1, 3, TIMESTAMP('2026-07-16 14:30:00')),
-(1, 2, 2, TIMESTAMP('2026-07-17 11:45:00')),
--- Bookings after 6 months
-(1, 6, 2, TIMESTAMP('2023-03-13 08:10:00')),
-(1, 7, 3, TIMESTAMP('2023-06-01 18:30:00')),
-(1, 8, 1, TIMESTAMP('2024-02-07 10:55:00')),
-(1, 9, 1, TIMESTAMP('2024-02-07 10:55:00')),
+(1, 3, 3, TIMESTAMP('2024-04-12 14:45:00')),
 -- Bookings for events that have past
-(1, 11, 3, TIMESTAMP('2024-10-23 17:20:00')),
-(1, 12, 2, TIMESTAMP('2024-11-03 12:35:00')),
-(1, 13, 1, TIMESTAMP('2024-12-08 09:45:00'));
+(1, 11, 3, TIMESTAMP('2023-10-20 17:20:00')),
+(1, 12, 2, TIMESTAMP('2023-10-03 12:35:00')),
+(1, 13, 1, TIMESTAMP('2023-01-08 09:45:00'));
+-- Bokings within 6 months
+(1, 4, 2, TIMESTAMP('2024-02-15 19:30:00'));
+(1, 3, 2, TIMESTAMP('2024-01-16 09:00:00'));
 
 
   -- ticket_id INT(8) ZEROFILL NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -205,12 +202,6 @@ INSERT INTO Ticket VALUES
 (00000010, 4, false),
 (00000011, 5, false),
 (00000012, 6, false),
-(00000013, 7, false),
-(00000014, 7, false),
-(00000015, 7, false),
-(00000016, 8, false),
-(00000017, 8, false),
-(00000018, 9, false);
 
 INSERT INTO Token VALUES
 (1, 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJldW5pY2VvbmcuMjAyMUBzY2lzLnNtdS5lZHUuc2ciLCJ1c2VyX2lkIjoxLCJyb2xlX2lkIjp7ImlkIjoxLCJuYW1lIjoiY3VzdG9tZXIifSwiaWF0IjoxNzExNTEzMjE1LCJleHAiOjE3MTE1OTk2MTV9.zh6RdIoKNMzJa7_yZ-vZHllJI7gAceMpwr1sLacHNB4', true, true, 1),
